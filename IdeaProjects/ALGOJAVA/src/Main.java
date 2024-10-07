9c924e4d0714ae9023393e1afba7088613643a50
siwar salmi <siwar.salmi@edu.esiee-it.fr>
2024-10-07 22:39:03 +0200

    tp2

--- IdeaProjects/ALGOJAVA/src/Main.java
+++ IdeaProjects/ALGOJAVA/src/Main.java
@@ -1,10 +1,44 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    //on declare un tableau de int nommé tab dune taille de 7 cases
+
+    static int [] Siwar_the_best = new int [7];
+
+
+    public static void affiche_tab(int [] tableau){
+        for (int i = 0; i < tableau.length ; i++) {
+            System.out.println("i = " + tableau[i]);
+        }
+    };
+
+
+
+
+    public static void max_tab(int [] tableau){
+        int max = tableau[0];
+        for (int i = 0; i < tableau.length ; i++) {
+            if (tableau[i] > max){
+                max = tableau[i];
+            }
+        }
+        System.out.println("le maximum = " + max);
+    };
+
+
+
+    public static boolean moins10 (int [] tableau) {
+        for (int j : tableau) {
+            if (j < 10) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+
+
 
     public static void main(String[] args) {
-        int [] Siwar_the_best = new int [7];
         Siwar_the_best[0]=5;
         Siwar_the_best[1]=15;
         Siwar_the_best[2]=25;
@@ -12,15 +46,10 @@ public class Main {
         Siwar_the_best[4]=45;
         Siwar_the_best[5]=55;
         Siwar_the_best[6]=65;
-
-        System.out.printf("Hello and welcome!");
-        float somme=0;
-
-        for (int i = 0; i < Siwar_the_best.length ; i++) {
-
-            System.out.println("i = " + Siwar_the_best[i]);
-            somme=somme+Siwar_the_best[i];
-        }
-        System.out.println("somme = "+somme/Siwar_the_best.length);
+        System.out.println("Hello and welcome!");
+        affiche_tab(Siwar_the_best);
+        max_tab(Siwar_the_best);
+        boolean f3 = moins10(Siwar_the_best);
+        System.out.println(f3);
     }
 }
